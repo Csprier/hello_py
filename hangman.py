9dc62ef4c41055760e9259119bf91d633eeebb91
Csprier <Csprier@gmail.com>
2023-10-09 19:16:58 -0400

    Fixed for loop in hangman() to account for length and error handling.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,12 @@
 # ===============================================================================================
+# ===== imports ===== #
+import random
+from collections import Counter
+
+
+# ===============================================================================================
+# INSTRUCTIONS
+# ---------------------------
 # create a greeting
 # create your word list
 # randomly choose a word from the list you have created
@@ -6,15 +14,12 @@
 # bonus make the program take the input from the user and make it lowercase
 # check if the letter is in the word
 
-# ===============================================================================================
-# ===== imports ===== #
-import random
-from collections import Counter
 
 # ===============================================================================================
 # Helper function
+# ---------------------------
 def rearrange_string(random_string, word):
-    # count the character frequencies in the random string and the word
+    # Count the character frequencies in the random string and the word
     random_string_count = Counter(random_string)
     word_count = Counter(word)
 
@@ -27,29 +32,44 @@ def rearrange_string(random_string, word):
 
 # ===============================================================================================
 # Main function
+# ---------------------------
 def hangman():
-    print("Greetings!")
-    word_list = ["pickles", "stank", "heck"]
-    random_word = random.choice(word_list)
+    # Create a greeting
+    print("====================================================================================================================")
+    print(" Greetings! To save this man's life, you must guess the correct letters of a random word you don't know! Good luck! ")
+    print("====================================================================================================================")
+
+    # Create word list
+    word_list = ["pickles", "stank", "heck", "meat", "bologna"]
     
+    # Randomly choose a word from the list you have created
+    random_word = random.choice(word_list)
+    print(random_word)
+    
+    # Ask the user to guess a letter
     user_guesses = []
     user_letters = ""
     
+    # Check if the letter is in the word
     for i in random_word:
-        letter_choice = input("Choose a letter! ").lower()
+        letter_choice = input("Choose a letter! ").lower() # Bonus - make the input lower case
         user_guesses.append(letter_choice)
+        # Make the list of letters into a single string
+        user_letters = "".join(user_guesses)
 
         if letter_choice in random_word:
-            print(f"'{letter_choice}' is part of the word!")
+            # If the length of the user's letters is the same length as the random_word, there are no more guesses required
+            if len(user_letters) != len(random_word):
+                print(f"'{letter_choice}' is part of the word! Guess again!")
+            else:
+                # User helper function to determine is the guessed letters can be rearranged to the random word
+                if (rearrange_string(user_letters, random_word) == True):
+                    print(f"Congratulations! The letters you chose spell the word '{random_word}'! The man will not be hanged!")
+                else:
+                    print(f"You failed to spell '{random_word}'. HANG HIM!")
+        # If the letter is not in the random_word, break out of the function
         else:
-            print(f"'{letter_choice}' is not part of the word!")
-
-    user_letters = "".join(user_guesses)
-
-    if (rearrange_string(user_letters, random_word) == True):
-        print(f"The letters you chose spell the word '{random_word}'! The man will not be hanged!")
-    else:
-        print(f"You failed to spell '{random_word}'. HANG HIM!")
-
+            print(f"'{letter_choice}' is not part of the word! You lose. He dies. Better luck next time.") 
+            break
 
 hangman()
