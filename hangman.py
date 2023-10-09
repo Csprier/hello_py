ea59f3cf5cdb2060f613b602de26f22cad7d446d
Csprier <Csprier@gmail.com>
2023-10-09 17:32:05 -0400

    created hangman.py and laid out instructions for the challenge.

---
+++ hangman.py
@@ -0,0 +1,8 @@
+# create a greeting
+# create your word list
+# randomly choose a word from the list you have created
+# ask the user to guess a letter
+# bonus make the program take the input from the user and make it lowercase
+# check if the letter is in the word
+
+ 
