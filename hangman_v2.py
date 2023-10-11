d31e0b98353f4f2486780548cd46ec337f2e4c69
Csprier <Csprier@gmail.com>
2023-10-11 15:56:32 -0400

    Formatted code and comments for clarity

--- hangman_v2.py
+++ hangman_v2.py
@@ -4,52 +4,42 @@ import random
 from collections import Counter
 
 # ===============================================================================================
-# Helper function
-# ---------------------------
-def rearrange_string(random_string, word):
-    # Count the character frequencies in the random string and the word
-    random_string_count = Counter(random_string)
-    word_count = Counter(word)
-
-    # Check if the character frequency in the random string are sufficient
-    for char, count in word_count.items():
-        if random_string_count[char] < count:
-            return False
-        
-    return True
-
-# ===============================================================================================
 # Main function
 # ---------------------------
 def hangman():
+    # ===============================================================================================
     # Create a greeting
-    print("====================================================================================================================")
-    print(" Greetings! To save this man's life, you must guess the correct letters of a random word you don't know! Good luck! ")
-    print("====================================================================================================================")
+    print("======================================================================================")
+    print(" Greetings! Welcome to Hangman! Save the man's life by guessing correctly! Good luck! ")
+    print("======================================================================================")
+    print("= Rules:")
+    print("= Players get 3 chances to guess.")
+    print("= There is no penalty for correct guesses, or repeated guesses.")
+    print("= Every missed letter is -1 chance. Get to 0 and the man hangs!")
 
+    # ===============================================================================================
     # Create word list
-    # word_list = ["pickles", "stank", "heck", "meat", "bologna", "apple", "banana"]
-    word_list = ["apple", "banana", "moot"]
+    word_list = ["pickles", "stank", "heck", "meat", "bologna", "apple", "banana"]
     
     # Randomly choose a word from the list you have created
     random_word = random.choice(word_list)
-    print(random_word)
     
     # Make a pre-existing dict of the word to cross reference
     random_word_dict = Counter(random_word)
-    print(f"random_word_dict: ", random_word_dict)
 
     # Make a counter for the user guesses key-value/ letter-frequency
     user_guesses = Counter()
 
     # How many chances a player has to guess
     chance_count = 3
-    
     # ===============================================================================================
     # Main loop
     # ===============================================================================================
     for i in random_word:
         while True:
+            # Display remaining chances
+            print(f"You have {chance_count} chances remaining!")
+
             # Has the user spelled the word with all their previous guesses?
                 # If yes, win the game
             if set(user_guesses.items()) == set(random_word_dict.items()):
@@ -58,18 +48,19 @@ def hangman():
             
             # If a user runs out of chances to guess, they lose!
             if chance_count == 0:
-                print(f"The man hangs because of your inability to guess correctly!")
+                print(f"The man hangs because of your inability to guess correctly! The word was {random_word}")
                 break
 
             # Ask the user to guess a letter
             guess = input("Choose a letter! ").lower() # Bonus - make the input lower case
+            
             # If the length of the guess is 1, continue.
             if len(guess) == 1:
                 # Have we guessed this letter already?
                     # If yes, user_guesses should reflect this with a higher frequency. Thus we let the user know they have already guessed this letter.
                     # If no, no penalty
                 if guess in user_guesses:
-                    print(f"You've guessed '{guess}' already! No penalty for this, but guess something else!")
+                    print(f"You've guessed '{guess}' already! Guess correctly before you run out of chances!")
                     continue
 
                 # Is the guess part of the random word?
@@ -78,19 +69,16 @@ def hangman():
                 if guess in random_word: # if yes
                     print(f"You successfully guessed a letter in the word!")
                     user_guesses[guess] = random_word_dict[guess]
-                    print(f"::: {user_guesses}")
+                    chance_count -= 1
                     continue
                 else: # if no
                     print(f"Sorry! That letter is not part of the word!")
-                    chance_count - 1
+                    chance_count -= 1
                     continue
             # If the length of the guess > 1, it can't be accepted as a valid input.
             else:
                 print("Single letters only.")
                 continue
-
-    print(f"user_guesses: ", user_guesses)
-    print(f"Chances remaining: {chance_count}")
 
     # ===============================================================================================
     # Play again or exit the game
@@ -101,18 +89,3 @@ def hangman():
         print("That's enough executions for one day. See you next time.")
 
 hangman()
-
-# # ===============================================================================================    
-# Check if the dict's are the same
-# are_equal = True
-# for key, value in user_guesses.items():
-#     if key in random_word_dict and random_word_dict[key] == value:
-#         continue
-#     else:
-#         are_equal = False;
-#         break
-
-# if are_equal == True:
-#     print(f"Congratulations! The letters you chose spell the word '{random_word}'! The man will not be hanged!")
-# else:
-#     print(f"You failed to spell '{random_word}'. HANG HIM!") 
